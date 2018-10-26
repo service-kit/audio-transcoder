a62b393d42093fa13bfedb10ad8ec6b31a0dbbe8
service-kit <likunting@ainirobot.com>
2018-10-26 15:49:10 +0800

    update

--- examples/pcm-to-mp3.go
+++ examples/pcm-to-mp3.go
@@ -2,52 +2,56 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"github.com/service-kit/audio-transcoder/pcm-transcoder"
 	"io"
-	"log"
 	"os"
+	"time"
 )
 
 func PcmToMp3() error {
 	f, e := os.OpenFile("../test.pcm", os.O_RDONLY, 0777)
 	if nil != e {
-		log.Println("test.pcm open err:", e.Error())
+		fmt.Println("test.pcm open err:", e.Error())
 		return e
 	}
 	defer f.Close()
 	buf := new(bytes.Buffer)
 	ii, e := io.Copy(buf, f)
 	if nil != e {
-		log.Println("copy file data err:", e.Error())
+		fmt.Println("copy file data err:", e.Error())
 		return e
 	}
-	log.Fatalln("file size:", ii)
+	fmt.Println("file size:", ii)
 	transcoder := new(pcm_transcoder.PcmMp3Transcoder)
 	afterTranscodingData, e := transcoder.Transcode(buf.Bytes())
 	if nil != e {
-		log.Println("transcode err:", e.Error())
+		fmt.Println("transcode err:", e.Error())
 		return e
 	}
 	of, e := os.OpenFile("../test_out.mp3", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
 	if nil != e {
-		log.Println("open out file err:", e.Error())
+		fmt.Println("open out file err:", e.Error())
 		return e
 	}
 	defer of.Close()
 	oi, e := of.Write(afterTranscodingData)
 	if nil != e {
-		log.Println("write file err:", e.Error())
+		fmt.Println("write file err:", e.Error())
 		return e
 	}
-	log.Println("write out data size:", oi)
+	fmt.Println("write out data size:", oi)
 	return nil
 }
 
 func main() {
 	e := PcmToMp3()
 	if nil != e {
-		log.Println("pcm to mp3 err:", e.Error())
+		fmt.Println("pcm to mp3 err:", e.Error())
 	} else {
-		log.Println("pcm to mp3 success")
+		fmt.Println("pcm to mp3 success")
+	}
+	for {
+		time.Sleep(time.Second)
 	}
 }
