b7848fd6614d985aa0e6ef3c28a28210e88f1a25
service-kit <likunting@ainirobot.com>
2018-10-26 15:44:14 +0800

    update

--- examples/pcm-to-mp3.go
+++ examples/pcm-to-mp3.go
@@ -8,40 +8,46 @@ import (
 	"os"
 )
 
-func PcmToMp3() {
+func PcmToMp3() error {
 	f, e := os.OpenFile("../test.pcm", os.O_RDONLY, 0777)
 	if nil != e {
-		log.Fatalln("test.pcm open err:", e.Error())
-		return
+		log.Println("test.pcm open err:", e.Error())
+		return e
 	}
 	defer f.Close()
 	buf := new(bytes.Buffer)
 	ii, e := io.Copy(buf, f)
 	if nil != e {
-		log.Fatalln("copy file data err:", e.Error())
-		return
+		log.Println("copy file data err:", e.Error())
+		return e
 	}
 	log.Fatalln("file size:", ii)
 	transcoder := new(pcm_transcoder.PcmMp3Transcoder)
 	afterTranscodingData, e := transcoder.Transcode(buf.Bytes())
 	if nil != e {
-		log.Fatalln("transcode err:", e.Error())
-		return
+		log.Println("transcode err:", e.Error())
+		return e
 	}
 	of, e := os.OpenFile("../test_out.mp3", os.O_WRONLY|os.O_CREATE|os.O_TRUNC, 0777)
 	if nil != e {
-		log.Fatalln("open out file err:", e.Error())
-		return
+		log.Println("open out file err:", e.Error())
+		return e
 	}
 	defer of.Close()
 	oi, e := of.Write(afterTranscodingData)
 	if nil != e {
-		log.Fatalln("write file err:", e.Error())
-		return
+		log.Println("write file err:", e.Error())
+		return e
 	}
-	log.Fatalln("write out data size:", oi)
+	log.Println("write out data size:", oi)
+	return nil
 }
 
 func main() {
-	PcmToMp3()
+	e := PcmToMp3()
+	if nil != e {
+		log.Println("pcm to mp3 err:", e.Error())
+	} else {
+		log.Println("pcm to mp3 success")
+	}
 }
